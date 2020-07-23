f23470584945da2384c0ed7201e84aa97257138a
Marcelo garcia soto <marcelo.algarciass@gmail.com>
2020-07-23 18:37:13 -0400

    added bash script to filter the output and aded output tf file

--- filter.sh
+++ filter.sh
@@ -3,3 +3,5 @@ function check_deps() {
   test -f $(which jq) || error_exit "jq command not detected in path, please install it"
 }
 check_deps
+OUTPUT=$(echo $1 | jq -r '.items[] | select(.mask | startswith("255"))' | grep "cidr" | awk '{print $2  }')
+jq -n --arg output "$OUTPUT"  "{\"cidr\":$OUTPUT}" 
